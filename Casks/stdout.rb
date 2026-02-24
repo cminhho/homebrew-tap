3747e69f507c49829e9a9e9e722858b493d4f50b
chung ho <chungho@AP-0324-1014.local>
2026-02-24 10:58:59 +0700

    chore(stdout): update cask to v1.1.3

--- Casks/stdout.rb
+++ Casks/stdout.rb
@@ -2,14 +2,14 @@
 # On new release of stdout: update version + sha256 (curl -sL <url> | shasum -a 256)
 
 cask "stdout" do
-  version "1.1.2"
+  version "1.1.3"
 
   on_intel do
-    sha256 "44b99f65d7bf82ffde72621f1c317eed11e39013608d6e2b6e0910999bb6b40e"
+    sha256 "aaad50ebe0bc931d0a4b052d461d75837049c837d1ed46b14e942910ade71da4"
     url "https://github.com/cminhho/stdout/releases/download/v#{version}/stdout-#{version}-mac.zip"
   end
   on_arm do
-    sha256 "a597955f950199e2838a17517362873e58fa23138926c7d8bed17d3df576b059"
+    sha256 "a776be3a5d560afb6ba0214ede48db6846a0dfcfbfacb26de4e549dbf1cbdd91"
     url "https://github.com/cminhho/stdout/releases/download/v#{version}/stdout-#{version}-arm64-mac.zip"
   end
 
