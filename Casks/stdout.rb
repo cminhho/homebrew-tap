29b5b2707ad1256bf63192436cf19a0572e0fbdb
chung ho <chungho@AP-0324-1014.fpt>
2026-02-25 15:14:40 +0700

    chore(homebrew-tap): bump stdout cask to v1.1.4

--- Casks/stdout.rb
+++ Casks/stdout.rb
@@ -2,14 +2,14 @@
 # On new release of stdout: update version + sha256 (curl -sL <url> | shasum -a 256)
 
 cask "stdout" do
-  version "1.1.3"
+  version "1.1.4"
 
   on_intel do
-    sha256 "aaad50ebe0bc931d0a4b052d461d75837049c837d1ed46b14e942910ade71da4"
+    sha256 "0fbe79fe6dc4da0292c14e240d13e9cb6cfc98833df46f46c5bc934516e97818"
     url "https://github.com/cminhho/stdout/releases/download/v#{version}/stdout-#{version}-mac.zip"
   end
   on_arm do
-    sha256 "a776be3a5d560afb6ba0214ede48db6846a0dfcfbfacb26de4e549dbf1cbdd91"
+    sha256 "d4f3b3647b5d6d64a690749922f543052acecf0b89ca098f779928078583ae96"
     url "https://github.com/cminhho/stdout/releases/download/v#{version}/stdout-#{version}-arm64-mac.zip"
   end
 
